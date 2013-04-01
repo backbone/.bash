0d92e06ba94084508c85f8f6d9a52381e1e6bc7d
Kolan Sh <mecareful@gmail.com>
2013-04-01 04:38:52 +0400

    TERM=screen genkernel alias.

--- alias.sh
+++ alias.sh
@@ -47,3 +47,4 @@ if [ `which grc 2>/dev/null` ]; then
 fi
 which iconv &>/dev/null && alias cp1251toUTF8='iconv -f cp1251 -t utf8'
 which eix &>/dev/null && alias eix='TERM=screen eix'
+which genkernel &>/dev/null && alias genkernel='TERM=screen genkernel'
